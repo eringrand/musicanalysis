52893d2c68118bb2b45560b41435255271d51b42
Justin Law <jhl2184@columbia.edu>
2015-05-01 15:35:42 -0400

    Update script to load kaggle data

--- justin.py
+++ justin.py
@@ -17,3 +17,21 @@ for i in range(len(data)):
 
 users = list(users)
 artists = list(artists)
+
+import numpy as np
+
+f = open('kaggle_visible_evaluation_triplets.txt', 'r')
+users = set()
+artists = set()
+
+for line in f:
+    split = line.split('\t')
+    userhash = split[0]
+    artisthash = split[1]
+    users.add(userhash)
+    artists.add(artisthash)
+
+n = len(users)
+m = len(artists)
+
+matrix = np.zeros((n,m))
