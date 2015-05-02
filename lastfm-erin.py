6d31c745eb1b3d93dc33db9729846cd30212418f
eringrand <eringrand@gmail.com>
2015-05-02 18:38:38 -0400

    make a matrix

---
+++ lastfm-erin.py
@@ -0,0 +1,81 @@
+
+# coding: utf-8
+
+# In[15]:
+
+import numpy as np
+from collections import defaultdict
+from scipy import sparse
+
+
+f = open('kaggle_visible_evaluation_triplets.txt', 'r')
+users = set()
+artists = set()
+numplays = {}
+artistdic = defaultdict(list)
+userdic = defaultdict(list)
+
+data = f.read().split('\n')
+
+
+for line in data:
+    split = line.strip().split('\t')
+    if len(split) != 3:
+        print len(split), split
+    else:
+        userhash = split[0]
+        artisthash = split[1]
+        plays = split[2]
+        users.add(userhash)
+        artists.add(artisthash)
+        numplays[artisthash, userhash] = plays
+        #if artisthash not in userdic[userhash]:
+        userdic[userhash].append(artisthash)
+        #if userhash not in artistdic[artisthash]:
+        artistdic[artisthash].append(userhash)
+        
+
+
+# In[16]:
+
+n = len(users)
+m = len(artists)
+
+users = list(users)
+artists = list(artists)
+
+user_dict = {}
+i = 0
+for user in users:
+    user_dict[user] = i
+    i += 1
+    
+artist_dict = {}
+i = 0
+
+for artist in artists:
+    artist_dict[artist] = i
+    i += 1
+
+
+# In[17]:
+
+M = sparse.lil_matrix((n, m))
+
+for user, artistlist in userdic.items():
+    useridx = user_dict[user]
+    for a in artistlist:
+        artistidx = artist_dict[a]
+        plays = numplays[a, user]
+        M[useridx, artistidx] = plays
+
+
+# In[18]:
+
+
+
+
+# In[ ]:
+
+
+
