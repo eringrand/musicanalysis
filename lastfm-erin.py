5d75506ee27a4c2a092108a0c3488aae48585c91
eringrand <eringrand@gmail.com>
2015-05-03 19:36:41 -0400

    new code

--- lastfm-erin.py
+++ lastfm-erin.py
@@ -1,17 +1,24 @@
+
+# coding: utf-8
+
+
+
 import numpy as np
 from collections import defaultdict
 from scipy import sparse
 
-
 f = open('kaggle_visible_evaluation_triplets.txt', 'r')
-users = set()
-artists = set()
-numplays = {}
-artistdic = defaultdict(list)
-userdic = defaultdict(list)
-
 data = f.read().split('\n')
 
+
+# In[2]:
+
+users = set()
+songs = set()
+numplays = {}
+numsongplayed = defaultdict(list)
+songdic = defaultdict(list)
+userdic = defaultdict(list)
 
 for line in data:
     split = line.strip().split('\t')
@@ -19,50 +26,115 @@ for line in data:
         print len(split), split
     else:
         userhash = split[0]
-        artisthash = split[1]
-        plays = split[2]
+        songhash = split[1]
+        plays = float(split[2])
         users.add(userhash)
-        artists.add(artisthash)
-        numplays[artisthash, userhash] = plays
-        #if artisthash not in userdic[userhash]:
-        userdic[userhash].append(artisthash)
-        #if userhash not in artistdic[artisthash]:
-        artistdic[artisthash].append(userhash)
+        songs.add(songhash)
+        numplays[songhash, userhash] = plays
+        if songhash not in userdic[userhash]:
+            userdic[userhash].append(plays)
+        if userhash not in songdic[songhash]:
+            songdic[songhash].append(userhash)
+        #if songhash not in numsongplayed:
+        numsongplayed[songhash].append(plays)
+
+
+# In[7]:
+
+# subset by number of songs a user has rated
+p1 = { key: value for key, value in userdic.items() if sum(value) > 29 }
+# subset by number of plays
+p2 = { key: value for key, value in numsongplayed.items() if sum(value) > 34 }
+
+# list of songs
+subsongs = p2.keys()
+
+# list of users who have rated those songs
+subusers = set()
+for s in subsongs:
+    for u in songdic[s]:
+        if u in p1:
+            subusers.add(u)
         
+p3 = { key: value for key, value in numplays.items() if key[0] in p2 and key[1] in p1 }
 
-n = len(users)
-m = len(artists)
+print len(subusers), len(users)
+print len(subsongs), len(songs)
+print 100. - (float(len(p3))/len(data) * 100)
+#print (float(len(subsongs)) * len(subusers)) / ( len(songs) * len(users)) * 100
 
-users = list(users)
-artists = list(artists)
+
+# In[8]:
+
+for key, value in p3.items():
+    song = key[0]
+    user = key[1]
+    playcounts = p2[song]
+    maxs = np.max(playcounts)
+    rng = maxs - 0.0
+    high = 1.0
+    low = 0.0 
+    normplay = high - (((high - low) * (maxs - value)) / rng)
+    p3[song, user] = normplay
+
+
+
+
+allkeys = p3.keys()
+
+np.random.shuffle(allkeys)
+
+trainingN = int(len(allkeys) * 0.8)
+trainingKEYS, testKEYS = allkeys[:trainingN], allkeys[trainingN:]
+
+print len(trainingKEYS), len(testKEYS)
+
+
+
+n = len(subusers)
+m = len(subsongs)
+
+users = subusers
+songs = subsongs
 
 user_dict = {}
 i = 0
-for user in users:
+for user in subusers:
     user_dict[user] = i
     i += 1
     
-artist_dict = {}
+song_dict = {}
 i = 0
-
-for artist in artists:
-    artist_dict[artist] = i
+for song in subsongs:
+    song_dict[song] = i
     i += 1
     
-M = sparse.lil_matrix((n, m))
+Mtrain = np.zeros((n,m))
+Mtest = np.zeros((n,m))
 
-for user, artistlist in userdic.items():
-    useridx = user_dict[user]
-    for a in artistlist:
-        artistidx = artist_dict[a]
-        plays = numplays[a, user]
-        M[useridx, artistidx] = plays
-    
-count = M.nonzero()
-count = len(count[0])
-print count
-print len(users)*len(artists)
-print float(count)/(len(users)*len(artists))
+for key in trainingKEYS:
+    plays = p3[key]
+    song = key[0]
+    user = key[1]
+    userindex = user_dict[user]
+    songindex = song_dict[song]
+    Mtrain[userindex, songindex] = float(plays)
 
+for key in testKEYS:
+    plays = p3[key]
+    song = key[0]
+    user = key[1]
+    userindex = user_dict[user]
+    songindex = song_dict[song]
+    Mtest[userindex, songindex] = float(plays)
+
+
+
+print np.sum(Mtrain, axis=0) 
+print np.sum(Mtest, axis=0) 
+
+print n, m
+print np.shape(Mtrain)
+print np.shape(Mtest)
 
 
