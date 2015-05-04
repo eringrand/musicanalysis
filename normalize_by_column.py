32b1bcbec84d4d442f1a2ddb0d39195bfcf98437
Jordan Rosenblum <jmrosen155@users.noreply.github.com>
2015-05-03 20:11:30 -0400

    Delete normalize_by_column.py

--- normalize_by_column.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def scale_linear_bycolumn(matrix, high=1.0, low=0.0):
-    mins = np.min(matrix, axis=0)
-    maxs = np.max(matrix, axis=0)
-    rng = maxs - mins
-    return high - (((high - low) * (maxs - matrix)) / rng)
