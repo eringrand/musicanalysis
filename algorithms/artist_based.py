87186984a637a88edb55d6c429fae98cc7b6c71d
Justin Law <jhl2184@columbia.edu>
2015-05-07 12:29:43 -0400

    Artist-based results and fixed algorithm

--- algorithms/artist_based.py
+++ algorithms/artist_based.py
@@ -142,13 +142,15 @@ popularsonglist = list(eval_songmeta.song_index.values)
 # apk calculation
 apk_sum = 0
 
-for key_user_index in list(set(omegau_test).intersection(user_songs.user_index.values)):
+for key_user_index in list(set(omegau_test).intersection(omegau_train)):
     songlist = []
     for key_artist_id in list(user_songs[user_songs.user_index==key_user_index].artist_id.values):
         songlist += artist_songdict[key_artist_id]
     # appending the popularsonglist to the generated songlist, assume 600 is more than enough to account for any overlaps
     songlist = list(OrderedDict.fromkeys(songlist + popularsonglist))[:600]
     # starting from np.delete, this function is just to remove the songs in songlist that already exist in omegau_train
-    apk_sum = apk(omegau_test[key_user_index],np.delete(songlist,np.nonzero(np.in1d(songlist,omegau_train[key_user_index])))[:500])
+    apk_sum += apk(omegau_test[key_user_index],np.delete(songlist,np.nonzero(np.in1d(songlist,omegau_train[key_user_index])))[:500])
 
-map = apk_sum/len(set(omegau_test).intersection(user_songs.user_index.values))
+map = apk_sum/len(list(set(omegau_test).intersection(omegau_train)))
+
+
